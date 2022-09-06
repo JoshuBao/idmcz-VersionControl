f79d51fd685c4daa78736d88221d3faccf878edf
idalis <dalibucks@gmail.com>
2022-09-06 09:18:37 -0700

    created index constructor

--- src/Index.java
+++ src/Index.java
@@ -1,4 +1,18 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class Index {
-
+	HashMap<String, Blob> hashie;
+	
+	public Index (String fileName) throws IOException {
+		FileWriter fw = new FileWriter("index");//output file
+		hashie = new HashMap <String, Blob>();
+		hashie.put(fileName, createBlob(fileName));
+	}
+	
+	public Blob createBlob (String fileName) throws IOException {
+		Blob newbie = new Blob (fileName);
+		return newbie;
+	}
 }
