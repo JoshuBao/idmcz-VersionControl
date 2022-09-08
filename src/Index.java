3b1373cacc583168ae3a565ce5df0f7fca424f15
idalis <dalibucks@gmail.com>
2022-09-08 12:45:50 -0700

    created removeBlob in index class

--- src/Index.java
+++ src/Index.java
@@ -1,26 +1,43 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 
 public class Index {
-	HashMap<String, Blob> hashie;
+	HashMap<String, Blob> objects;
 	FileWriter fw;
 	
 	public Index (String fileName) throws IOException {
+		initialize ();
+	}
+	
+	public void initialize () throws IOException {
 		fw = new FileWriter("index");//output file
-		hashie = new HashMap <String, Blob>();
-		
+		File theDir = new File("/path/objects");
+		if (!theDir.exists()){
+		    theDir.mkdirs();
+		}
+		objects = new HashMap <String, Blob>();
 	}
 	
 	public void addBlob (String fileName) throws IOException {
 		Blob newbie = new Blob (fileName);
-		hashie.put(fileName, newbie);
-		
+		objects.put(fileName, newbie);
 		fw.append(fileName);
-		fw.append(newbie.name());
+		fw.append(" : " + newbie.name());
 	}
 	
-	public void removeBlob (String fileName) {
-		hashie.remove(fileName);
+	public void writeIndex () throws IOException {
+		for (String str : objects.keySet()) {
+			fw.append(str);
+			fw.append(" : " + objects.get(str).name());
+		}
+	}
+	
+	public void removeBlob (String fileName) throws IOException {
+		FileWriter fw2 = new FileWriter("./objects/"+objects.get(fileName).name());
+		fw2.close();
+		objects.remove(fileName);
+		writeIndex ();
 	}
 }
